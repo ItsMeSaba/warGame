de912d1af4e6c9bb26d0634f59a27331bda20401
Generalsimus <totorotomoney@gmail.com>
2022-10-09 01:05:05 +0400

    config

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import "../styles.css"
+import "./style.scss"
 
 const canvas = document.querySelector("#main") as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,89 @@
-// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-const isProduction = process.env.NODE_ENV == 'production';
-
-
-const stylesHandler = 'style-loader';
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 
 
-const config = {
-    entry: './src/index.ts',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-    },
-    devServer: {
-        open: true,
-        host: 'localhost',
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: 'index.html',
-        }),
+module.exports = function (env, argv) {
+    const isEnvDevelopment = argv.mode === 'development';
+    const publicDirectory = path.resolve(__dirname, "public");
+    const outputDirectory = path.resolve(__dirname, "dist");
+    const indexHtmlFile = path.resolve(__dirname, "index.html");
+    const tsconfigFile = path.resolve(__dirname, "tsconfig.json");
+    const devPort = parseInt(argv.port) || 2222
 
-        // Add your plugins here
-        // Learn more about plugins from https://webpack.js.org/configuration/plugins/
-    ],
-    module: {
-        rules: [
-            {
-                test: /\.(ts|tsx)$/i,
-                loader: 'ts-loader',
-                exclude: ['/node_modules/'],
-            },
-            {
-                test: /\.css$/i,
-                use: [stylesHandler,'css-loader'],
-            },
-            {
-                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-                type: 'asset',
-            },
 
-            // Add your rules for custom modules here
-            // Learn more about loaders from https://webpack.js.org/loaders/
+    return {
+        entry: {
+            index: "./index.ts",
+        },
+        module: {
+            rules: [
+                {
+                    test: /.(ts|((t|j)sx)|json)$/i,
+                    use: [
+                        {
+                            loader: "ts-loader",
+                            options: {
+                                configFile: tsconfigFile,
+                                compilerOptions: {
+                                    sourceMap: isEnvDevelopment,
+                                },
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /.(s?c|sa)ss$/i,
+                    use: [
+                        { loader: "style-loader" },
+                        {
+                            loader: "css-loader",
+                            options: {
+                                sourceMap: isEnvDevelopment,
+                                // for use css modules names 'import module from "./style.css"'
+                                // modules: true,
+                                url: true,
+                                import: true,
+                            }
+                        },
+                        { loader: "postcss-loader" },
+                        { loader: "sass-loader" },
+                    ]
+                }
+            ],
+        },
+        resolve: {
+            extensions: [".tsx", ".ts", ".js", ".jsx", ".json"],
+        },
+        output: {
+            filename: "[chunkhash].bundle.js",
+            path: outputDirectory,
+            clean: true,
+            asyncChunks: true,
+            publicPath: "/"
+        },
+        devServer: {
+            static: publicDirectory,
+            historyApiFallback: true,
+            port: devPort,
+            open: true,
+            hot: true,
+        },
+        devtool: isEnvDevelopment ? "source-map" : undefined,
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: indexHtmlFile,
+            }),
+            new CopyWebpackPlugin({
+                patterns: [{ from: publicDirectory, to: outputDirectory }],
+            }),
+            new ForkTsCheckerWebpackPlugin({
+                typescript: { configFile: tsconfigFile },
+                devServer: isEnvDevelopment
+            })
         ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.jsx', '.js', '...'],
-    },
-};
-
-module.exports = () => {
-    if (isProduction) {
-        config.mode = 'production';
-        
-        
-    } else {
-        config.mode = 'development';
-    }
-    return config;
-};
+    };
+}
